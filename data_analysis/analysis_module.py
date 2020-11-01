8413973be961ef17bbf337f9a899c36d97ae8ec2
yhao21 <13701304462@163.com>
2020-11-01 01:14:54 -0400

    data

--- data_analysis/analysis_module.py
+++ data_analysis/analysis_module.py
@@ -71,6 +71,16 @@ class DataClean():
         We only need round = 1 and 2.
 
         This function will delete round 3 and 5 for you.
+
+
+        example:
+        coin_df = pd.read_csv('CoinMKT_48hrs_data.csv')
+        gecko_df = pd.read_csv('Gecko_48hrs_data.csv')
+        
+        coin_bitcoin = coin_df[coin_df['name'] == 'bitcoin']
+        gecko_bitcoin = gecko_df[gecko_df['name'] == 'bitcoin']
+
+        coin_clean, gecko_clean = DataClean(coin_bitcoin, gecko_bitcoin).clean_rows()
         '''
 
         self.remove_rows()
@@ -101,6 +111,93 @@ class DataClean():
         return self.coin_df, self.gecko_df
 
 
+class ComparableData():
+    def __init__(self, df_base, df_compare, compare_column):
+        self.df_base = df_base
+        self.df_compare = df_compare
+        # compare_column is a number, in this case, comp_col = abbr of each currency
+        self.col = compare_column
+        self.data = []
+        self.data_backup = None
+
+
+    def grouping(self):
+
+        # col 1 = repetition
+        # need to find the max value of repetition
+        rep_list = self.df_base.iloc[:, 1].values
+        max_rep = max(rep_list)
+        min_rep = min(rep_list)
+
+        # get abbr from df with repetition == 2
+        # len = 500
+        base_list = list(self.df_base[self.df_base['repetition'] == 2].iloc[:, self.col].values) 
+        comp_list = list(self.df_base[self.df_base['repetition'] == 3].iloc[:, self.col].values) 
+        self.matching(base_list, comp_list)
+
+        for rep in range(min_rep, max_rep + 1):
+            # find dataframe with repetition == rep
+            comp_list = self.next_repetition(self.df_base, rep)
+            # backup self.data to self.data_backup, use it in method 'self.matching'
+            self.data_backup = self.data
+            # clear all content in self.data
+            self.data = []
+            self.matching(self.data_backup, comp_list)
+
+        coin_data = self.data
+
+        self.data = []
+        base_list = list(self.df_compare[self.df_compare['repetition'] == 2].iloc[:, self.col].values) 
+        comp_list = list(self.df_compare[self.df_compare['repetition'] == 3].iloc[:, self.col].values) 
+        self.matching(base_list, comp_list)
+
+        for rep in range(min_rep, max_rep + 1):
+            # find dataframe with repetition == rep
+            comp_list = self.next_repetition(self.df_compare, rep)
+            # backup self.data to self.data_backup, use it in method 'self.matching'
+            self.data_backup = self.data
+            # clear all content in self.data
+            self.data = []
+            self.matching(self.data_backup, comp_list)
+
+        gecko_data = self.data
+
+
+        self.data = []
+        self.matching(coin_data, gecko_data)
+        coin_comparable = self.data
+
+
+        # len(coin_data) = 81, based on current data from coinmktcap,
+        # 81 currencies appear in each repetition.
+        # len(gecko_data) = 449, based on current data from gecko,
+        # 449 currencies appear in each repetition
+
+        return self.data
+
+
+
+    def matching(self, base_list, comp_list):
+        '''
+        Compile currencies appear in both list,
+        save matching currencies to self.data
+        '''
+        for item in comp_list:
+            # must include condition: not in self.data to avoid same abbr in self.data
+            if item in base_list and item not in self.data:
+                self.data.append(item)
+
+
+    def next_repetition(self, df, rep):
+        '''
+        Return new dataframe with repetition == rep in df
+        i.e., a = self.next_repetition(self.df_base, 1)
+
+        Use for loop get rep == i, i = 1, 2, 3,... 192, from coin and gecko df.
+        '''
+        
+        return df[df['repetition'] == rep].iloc[:, self.col].values
+        
 
 
 
@@ -113,8 +210,12 @@ if __name__ == '__main__':
     coin_df = pd.read_csv('CoinMKT_48hrs_data.csv')
     gecko_df = pd.read_csv('Gecko_48hrs_data.csv')
     
-    coin_bitcoin = coin_df[coin_df['name'] == 'bitcoin']
-    gecko_bitcoin = gecko_df[gecko_df['name'] == 'bitcoin']
+    # coin_rep2 = coin_df with repetition == 2
+    # shape = (500, 10)
+    #coin_rep2 = coin_df[coin_df['repetition'] == 2]
+    #print(coin_rep2.iloc[:, 4].values)
 
-    coin_clean, gecko_clean = DataClean(coin_bitcoin, gecko_bitcoin).clean_rows()
+    data_comparable = ComparableData(coin_df, gecko_df, 4).grouping()
+    print('len data_comparable: ', len(data_comparable))
+    print(data_comparable)
 
