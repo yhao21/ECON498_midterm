e7b6ae7bedda067eebdda84d3f9ac45ae4e8252f
yhao21 <13701304462@163.com>
2021-03-30 16:27:16 -0400

    new

--- MainConsole.py
+++ MainConsole.py
@@ -13,11 +13,11 @@ from deeplink_parsing import ParsingDeepLink
 # Step 1. Download 48hr data
 #==============================================================================
 
-url_coin_base = 'https://coinmarketcap.com/' 
-url_gecko_base = 'https://www.coingecko.com/en?page=' 
-url_list = [(url_coin_base + str(i), url_gecko_base + str(i)) for i in range(1,6)]
-folder_name = ['coinmktcap_html_file', 'gecko_html_file']
-Scrapping(url_list, folder_name, 48).folder_setup()
+#url_coin_base = 'https://coinmarketcap.com/' 
+#url_gecko_base = 'https://www.coingecko.com/en?page=' 
+#url_list = [(url_coin_base + str(i), url_gecko_base + str(i)) for i in range(1,6)]
+#folder_name = ['coinmktcap_html_file', 'gecko_html_file']
+#Scrapping(url_list, folder_name, 48).folder_setup()
 
 
 #==============================================================================
@@ -29,12 +29,12 @@ Parsing(folder).parsing_coin_html()
 folder = 'gecko_html_file'
 Parsing(folder).parsing_gecko_html()
 
-deeplink_list = pair_deeplink()
-
-print('=' * 100 + '\nFinish pairing' + ' [ ' + str(len(deeplink_list)) + ' ] ' + 'URLs...\n' + '=' * 100)
-
-file_names = etr(deeplink_list)
-
+#deeplink_list = pair_deeplink()
+#
+#print('=' * 100 + '\nFinish pairing' + ' [ ' + str(len(deeplink_list)) + ' ] ' + 'URLs...\n' + '=' * 100)
+#
+#file_names = etr(deeplink_list)
+#
 
 
 
@@ -46,8 +46,8 @@ file_names = etr(deeplink_list)
 ### 0.25 for 15 mins. Recall, for loop repeat every 15 mins, and unit of 0.25 is hour. repetition = 0.25*4 = 1 round
 ### hence, we only need to download these deeplink for once, we need 0.25*4 = 1
 
-deeplink_folder = ['coin_500deeplink', 'gecko_500deeplink']
-DeepLink(deeplink_list, deeplink_folder,file_names).folder_setup()
+#deeplink_folder = ['coin_500deeplink', 'gecko_500deeplink']
+#DeepLink(deeplink_list, deeplink_folder,file_names).folder_setup()
 
 
 
@@ -55,12 +55,12 @@ DeepLink(deeplink_list, deeplink_folder,file_names).folder_setup()
 #==============================================================================
 # Step 4. Parsing deeplinks (!!!! Need to add another parsing structure in coinmktcap deeplink)
 #==============================================================================
-folder = 'coin_500deeplink'
-ParsingDeepLink(folder).parsing_coin_deeplink()
-folder = 'gecko_500deeplink'
-ParsingDeepLink(folder).parsing_gecko_deeplink()
-
-
+#folder = 'coin_500deeplink'
+#ParsingDeepLink(folder).parsing_coin_deeplink()
+#folder = 'gecko_500deeplink'
+#ParsingDeepLink(folder).parsing_gecko_deeplink()
+#
+#
 
 
 
@@ -68,16 +68,16 @@ ParsingDeepLink(folder).parsing_gecko_deeplink()
 #==============================================================================
 # Step 8. Parse logo img
 #==============================================================================
-folder = ['coin_500deeplink', 'gecko_500deeplink']
-save_logo_url(folder)
+#folder = ['coin_500deeplink', 'gecko_500deeplink']
+#save_logo_url(folder)
 
 
 
 #==============================================================================
 # Step 9. Logo img scrapping
 #==============================================================================
-csv_files = ['coin_logo.csv', 'gecko_logo.csv']
-logo_scrapping(csv_files)
+#csv_files = ['coin_logo.csv', 'gecko_logo.csv']
+#logo_scrapping(csv_files)
 
 
 
@@ -88,12 +88,12 @@ logo_scrapping(csv_files)
 #==============================================================================
 # re-download data from Error_Log.csv
 
-check_folder()
-
-folder = 'coin_500deeplink'
-ParsingDeepLink(folder).parsing_coin_deeplink()
-folder = 'gecko_500deeplink'
-ParsingDeepLink(folder).parsing_gecko_deeplink()
+#check_folder()
+#
+#folder = 'coin_500deeplink'
+#ParsingDeepLink(folder).parsing_coin_deeplink()
+#folder = 'gecko_500deeplink'
+#ParsingDeepLink(folder).parsing_gecko_deeplink()
 
 
 
